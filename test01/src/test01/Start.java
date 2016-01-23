a174c40f1eaf7f36273a7cbbe1cb7e9baa46fd35
escheung <escheung@ualberta.ca>
2016-01-23 07:10:12 -0700

    First Commit of c690 Test Project

---
+++ test01/src/test01/Start.java
@@ -0,0 +1,37 @@
+package test01;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import opennlp.tools.sentdetect.*;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		InputStream in = null;
+		try {
+			in = new FileInputStream("en-sent.bin");
+			SentenceModel model = new SentenceModel(in);
+			SentenceDetectorME detector = new SentenceDetectorME(model);
+			String sentences[] = detector.sentDetect("Hello! I am Eric.");
+			
+			for (int i=0; i<sentences.length; i++) {
+				System.out.println(sentences[i]);
+			}
+			
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException ioe) {
+					
+				}
+			}
+		}
+	}
+
+}
